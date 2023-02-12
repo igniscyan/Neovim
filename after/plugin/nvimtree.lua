3c2f58e508206eaa54a1a519f3fd2233ecbcb1a8
Brandon Clark <bwclark97@gmail.com>
2023-02-12 18:29:26 -0500

    Fixed sumaneko_lua references, fixed NvimTree regression.

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -9,3 +9,18 @@ vim.opt.termguicolors = true
 
 -- empty setup using defaults
 require("nvim-tree").setup()
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
